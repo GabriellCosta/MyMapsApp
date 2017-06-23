d496442b7ff365b9e3ef5431159bfd66cf2139ad
Gabriel Costa <gabrielcosta08@gmail.com>
2017-06-23 01:09:25 -0300

    Implement MapFragment on MainActivity

--- app/src/main/java/com/gabriellcosta/mymapsapp/MainActivity.java
+++ app/src/main/java/com/gabriellcosta/mymapsapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.gabriellcosta.mymapsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+  private static final String TAG = "MainActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.fragment_main_map);
+
+    mapFragment.getMapAsync(this);
+  }
+
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    Log.v(TAG , "Map Ready to be used");
   }
 }
