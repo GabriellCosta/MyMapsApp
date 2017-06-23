cc7eed6f3d84f537af59320b9b9d4906839f0608
Gabriel Costa <gabrielcosta08@gmail.com>
2017-06-23 02:09:15 -0300

    Add Util class to launch app on Tests
    Launch app using UIAutomator

--- app/src/androidTest/java/com/gabriellcosta/mymapsapp/AppLauncherUtil.java
+++ app/src/androidTest/java/com/gabriellcosta/mymapsapp/AppLauncherUtil.java
@@ -1,9 +1,43 @@
 package com.gabriellcosta.mymapsapp;
 
-/**
- * Created by gabrielcosta on 23/06/17.
- */
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
 
-public class AppLauncherUtil {
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
+
+public final class AppLauncherUtil {
+
+  private static final int LAUNCH_TIMEOUT = 5000;
+
+  private AppLauncherUtil() {
+  }
+
+  public static void launchApp(final UiDevice device) {
+    // Start from the home screen
+    device.pressHome();
+
+    // Wait for launcher
+    final String launcherPackage = device.getLauncherPackageName();
+    assertThat(launcherPackage, notNullValue());
+    device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+        LAUNCH_TIMEOUT);
+
+    // Launch the app
+    Context context = InstrumentationRegistry.getContext();
+    final Intent intent = context.getPackageManager()
+        .getLaunchIntentForPackage(BuildConfig.APPLICATION_ID);
+    // Clear out any previous instances
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    context.startActivity(intent);
+
+    // Wait for the app to appear
+    device.wait(Until.hasObject(By.pkg(BuildConfig.APPLICATION_ID).depth(0)),
+        LAUNCH_TIMEOUT);
+  }
 
 }
